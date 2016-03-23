4333767bb6397d1e0da15835e70b2bf8869ddc04
Trotyl Yu <yzj1995@vip.qq.com>
2016-03-23 22:46:50 +0800

    Remove tests code.

--- ts_in_ts/app/app.component.ts
+++ ts_in_ts/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, Directive, AfterContentInit } from 'angular2/core';
-import { NgFor } from 'angular2/common';
+import { Component } from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
 import { HeroService } from './hero.service';
@@ -7,71 +6,43 @@ import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
-@Directive({ 
-    selector: '[ngFor][ngForOf]', 
-    inputs: ['ngForTrackBy', 'ngForOf', 'ngForTemplate'] 
-})
-class NgForIntercepter implements AfterContentInit {
-    ngForOf;
-    ngForTrackBy;
-    ngForTemplate;
-    
-    ngAfterContentInit() {
-        console.log(this.ngForOf);
-        console.log(this.ngForTrackBy);
-        console.log(this.ngForTemplate);
-    }
-    
-    constructor() {
-        console.log(this.ngForOf);
-        console.log(this.ngForTrackBy);
-        console.log(this.ngForTemplate);
-    }
-}
-
 @Component({
-  selector: 'my-app',
-  template: `
+    selector: 'my-app',
+    template: `
     <h1>{{title}}</h1>
     <nav>
       <a [routerLink]="['Dashboard']">Dashboard</a>
       <a [routerLink]="['Heroes']">Heroes</a>
     </nav>
-    <div *ngFor="#name of names">{{ name }}{{ index }}</div>
     <router-outlet></router-outlet>
   `,
-  styleUrls: ['app/app.component.css'],
-  directives: [ROUTER_DIRECTIVES, NgFor, NgForIntercepter],
-  providers: [
-    ROUTER_PROVIDERS,
-    HeroService
-  ]
+    styleUrls: ['app/app.component.css'],
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
+        ROUTER_PROVIDERS,
+        HeroService
+    ]
 })
 @RouteConfig([
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: DashboardComponent,
-    useAsDefault: true
-  },
-  {
-    path: '/detail/:id',
-    name: 'HeroDetail',
-    component: HeroDetailComponent
-  },
-  {
-    path: '/heroes',
-    name: 'Heroes',
-    component: HeroesComponent
-  }
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
+    },
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+    }
 ])
 export class AppComponent {
-  title = 'Tour of Heroes';
-  names: string[];
-  
-  constructor() {
-      this.names = ['Alice', 'Bob', 'Cindy', 'Dave', 'ET', 'France'];
-  }
+    title = 'Tour of Heroes';
 }
 
 
